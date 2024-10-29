4adafde6c1f88698760ad166f17994e7a39a2482
Jim Lawless <lawless.jim@gmail.com>
2024-10-29 18:30:05 -0500

    Add files via upload

---
+++ aoc3b.go
@@ -0,0 +1,94 @@
+// Jim Lawless
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"os"
+)
+
+const MAX_CPL = 1000
+
+func main() {
+	file, err := os.Open(os.Args[1])
+	handleError(err)
+	defer func() {
+		if err = file.Close(); err != nil {
+			log.Fatal(err)
+		}
+	}()
+	scanner := bufio.NewScanner(file)
+	var lines []string
+	for scanner.Scan() {
+		lines = append(lines, scanner.Text())
+	}
+	sum := 0
+	positions := make(map[int]int)
+
+	for i := 0; i < len(lines); i++ {
+		s := lines[i]
+		accum := 0
+		symFound := false
+
+		pos := 0
+		for j := 0; j < len(s); j++ {
+			if isDigit(s[j]) {
+				accum = accum*10 + int(s[j]-'0')
+				symFound = symFound || adjacentSym(&pos, lines, i-1, j-1)
+				symFound = symFound || adjacentSym(&pos, lines, i-1, j)
+				symFound = symFound || adjacentSym(&pos, lines, i-1, j+1)
+				symFound = symFound || adjacentSym(&pos, lines, i, j+1)
+				symFound = symFound || adjacentSym(&pos, lines, i+1, j+1)
+				symFound = symFound || adjacentSym(&pos, lines, i+1, j)
+				symFound = symFound || adjacentSym(&pos, lines, i+1, j-1)
+				symFound = symFound || adjacentSym(&pos, lines, i, j-1)
+
+			} else {
+				if symFound {
+					if positions[pos] != 0 {
+						sum += accum * positions[pos]
+					} else {
+						positions[pos] = accum
+					}
+				}
+
+				accum = 0
+				symFound = false
+			}
+		}
+		if symFound {
+			if positions[pos] != 0 {
+				sum += accum * positions[pos]
+			} else {
+				positions[pos] = accum
+			}
+		}
+	}
+	fmt.Println(sum)
+}
+
+func isDigit(b byte) bool {
+	return b >= '0' && b <= '9'
+}
+
+func adjacentSym(pos *int, lin []string, row int, col int) bool {
+	if row < 0 || row >= len(lin) {
+		return false
+	}
+	if col < 0 || col >= len(lin[row]) {
+		return false
+	}
+	tmp := lin[row][col]
+	if tmp == '*' {
+		*pos = row*MAX_CPL + col
+		return true
+	}
+	return false
+}
+func handleError(e error) {
+	if e != nil {
+		log.Fatal(e)
+		os.Exit(1)
+	}
+}
